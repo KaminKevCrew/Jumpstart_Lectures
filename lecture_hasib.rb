277db9facd6267817b5a3d1f874823861d3aa46a
KaminKevCrew <durgeisawesome@gmail.com>
2020-03-10 20:30:12 -0700

    Add count as

--- lecture_hasib.rb
+++ lecture_hasib.rb
@@ -1,25 +1,37 @@
-# def count_a(str)
-# end
+#take a string and return a count of all 'a' or 'A'
+def count_a(str)
+  counter = 0
+  i = 0
+  while i < str.length
+    word = str[i]
+    if word == 'a' || word == 'A'
+      counter += 1
+    end
+    i += 1
+  end
+  return counter
+end
 
-# p count_a("avatar aang") # 5
-# p count_a("Avatar Aang") # 5
-# p count_a("Abstemiously") # 1
-# p count_a("Mississippi") # 0
+p count_a("avatar aang") # 5
+p count_a("Avatar Aang") # 5
+p count_a("Abstemiously") # 1
+p count_a("Mississippi") # 0
 
 # write a method is_prime? that takes in an number and returns true if
 # the number is prime, false otherwise
 
-# def is_prime?(number)
-# end
+def is_prime?(number)
+  
+end
 
-# p is_prime?(2) # true
-# p is_prime?(3) # true
-# p is_prime?(97) # true
-# p is_prime?(1) # false
-# p is_prime?(4) # false
-# p is_prime?(9) # false
-# p is_prime?(121) # false
-# p is_prime?(43.5) # false
+p is_prime?(2) # true
+p is_prime?(3) # true
+p is_prime?(97) # true
+p is_prime?(1) # false
+p is_prime?(4) # false
+p is_prime?(9) # false
+p is_prime?(121) # false
+p is_prime?(43.5) # false
 
 # write a method all_primes_to that takes in a number and prints all primes
 # up to that number
