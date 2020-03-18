c68f0f6c62f336dab7c1c7f59af94ee85658d2bc
KaminKevCrew <durgeisawesome@gmail.com>
2020-03-17 18:42:49 -0700

    quick commit

--- lecture_hasib.rb
+++ lecture_hasib.rb
@@ -12,10 +12,10 @@ def count_a(str)
   return counter
 end
 
-p count_a("avatar aang") # 5
-p count_a("Avatar Aang") # 5
-p count_a("Abstemiously") # 1
-p count_a("Mississippi") # 0
+# p count_a("avatar aang") # 5
+# p count_a("Avatar Aang") # 5
+# p count_a("Abstemiously") # 1
+# p count_a("Mississippi") # 0
 
 # write a method is_prime? that takes in an number and returns true if
 # the number is prime, false otherwise
@@ -42,19 +42,28 @@ def is_prime?(number)
   return true
 end
 
-p is_prime?(2) # true
-p is_prime?(3) # true
-p is_prime?(97) # true
-p is_prime?(1) # false
-p is_prime?(4) # false
-p is_prime?(9) # false
-p is_prime?(121) # false
+# p is_prime?(2) # true
+# p is_prime?(3) # true
+# p is_prime?(97) # true
+# p is_prime?(1) # false
+# p is_prime?(4) # false
+# p is_prime?(9) # false
+# p is_prime?(121) # false
 
 # write a method all_primes_to that takes in a number and prints all primes
 # up to that number
-# HINT: use our is_prime helper method!
+# HINT: use our is_prime? helper method!
 
-#   def all_primes_to(number)
-#   end
+def all_primes_to(number)
+  prime_arr = []
+  i = 0
+  while i <= number
+    if is_prime?(i)
+      prime_arr << i
+    end
+    i += 1
+  end
+  return prime_arr
+end
 
-# p all_primes_to(20) #2,3,5,7,11,13,17,19
+p all_primes_to(20) #2,3,5,7,11,13,17,19
