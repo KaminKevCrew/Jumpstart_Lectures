bba6fb91146f6e08cdd73754b88f79f001ec5839
KaminKevCrew <durgeisawesome@gmail.com>
2020-05-11 19:36:37 -0700

    Add lecture for W1D1

---
+++ may_data_types.rb
@@ -0,0 +1,126 @@
+############
+# # Data Types
+############
+
+# # intro to p
+# p "Hello World"
+
+
+
+# # numbers
+
+# p 1 + 1
+# p 2 + 2
+# p 2 - 1
+# p 4 * 1
+# p 5 / 2
+# p 11 / 3
+
+# p 11 % 3
+
+# 1 + 2 * 3 / 6 # order of operations
+
+
+# # booleans
+
+# # # consists of true and false
+# true
+# false
+# 1 + 4 == 5
+
+# # == (check equality)
+# p (2 * 4) + 1 == 3 * 3
+
+# # != (check inequality)
+# p 4 != "hi"
+
+# # < and > (greater than, less than)
+# p "cat" < "elephant"
+
+# # <= and >= (less than or equal to)
+# p 1 <= 1
+# p 0.5 ** 5
+
+# # ! ('bang' operator)
+# p !true # false
+# p !false # true
+# p !( 2 != 4)
+
+# # && ('and' operator)
+# p "string".length > 100 && 2 == 2
+
+
+# # || ('or' operator)
+# p false || false || false || 2 == 5 || true
+
+# # can also use 'or'
+# p true or false
+
+
+############
+# # Strings
+############
+
+# p 'Jumpstart is dope!'
+
+# p "1" == 1
+
+# p "1" + "asdfa3" + "hello" # "1a"
+# p "kevin".length
+# p "          ".length
+# p "jumpstart is cool"[2]
+# p "abcd"[0]
+# p "abcd"[1]
+# p "abcd"[2]
+# p "abcd"[3]
+# p "abcd"[4] # ???????
+
+# 1 + "2"
+
+# p "abcd"[-1] # "abcd"["abcd".length-1]
+# p "abcd"[-2]
+# p "abcd"[-4]
+# "ernie is here"[-1]
+# p "jumpstart is cool"[1..4]
+# p "jumpstart is cool"[0..3]
+# p "jumpstart is cool"[-4..-1]
+
+
+# # # variables
+
+# aj_boolean = "abc"
+
+# p aj_boolean
+
+# my_variable = "kevin falls asleep"
+# p my_variable
+
+# my_variable += "???"
+
+# p "#{my_variable}???"
+
+# num = 5
+# num += 3 # num = num + 3
+# p num
+
+# num = 7
+# p num
+# num -= 10
+# p num
+# temp = "#{5 + 5 / 7}!!!!!!"
+# p temp
+# temp = 5 + 5 / 7
+# p temp
+
+# # syntactic sugar for manipulating variables
+
+# sum = 10
+
+# p sum -= 1
+# #  sum = sum - 1
+# p sum *= 2
+# # sum = sum * 2
+# p sum /= 1
+# # # sum = sum / 1
+
+# p sum
