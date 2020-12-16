0b8f05099055bb1cbceacecf4b5841c85cc90917
KaminKevCrew <kaminskikeving@gmail.com>
2020-12-15 19:22:15 -0800

    add vowel remover

---
+++ vowel_remover.rb
@@ -0,0 +1,11 @@
+# define a method, vowel_remover that takes in an array of strings
+# and returns a new array where every string has all of its vowels removed.
+
+def vowel_remover(arr)
+
+end
+
+p vowel_remover(["Aardvark", "blight", "chimera"]) 
+#=> ["dvrk", "blght", "chmr"]
+p vowel_remover(["Dilapidated", "enervate", "ferocious", "McMaster"]) 
+#=> ["Dlpdtd", "nrvt", "frcs", "McMstr"]
